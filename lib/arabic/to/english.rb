57d5feeb3ce5e88bdf9a35e31bfd1e0d27fc7206
goreorto <cesar.cejudo@gmail.com>
2015-06-07 11:39:01 -0500

    Converts decimal base

--- lib/arabic/to/english.rb
+++ lib/arabic/to/english.rb
@@ -3,9 +3,55 @@ module Arabic
     module English
 
 		def convert number
+
+			case number
+
+			when 1
+
+				"one"
+
+			when 2
+
+				"two"
+
+			when 3
+
+				"three"
+
+			when 4
+
+				"four"
+
+			when 5
+
+				"five"
+
+			when 6
+
+				"six"
+
+			when 7
+
+				"seven"
+
+			when 8
+
+				"eight"
+
+			when 9
+
+				"nine"
+
+			when 10
+
+				"ten"
+
+			end
+
 		end
 
 		module_function :convert
+
     end
   end
 end
--- spec/arabic/to/english_spec.rb
+++ spec/arabic/to/english_spec.rb
@@ -2,4 +2,32 @@ require "arabic/to/english"
 
 describe Arabic::To::English do
 
+	describe "base number" do
+
+		it {
+
+			expect( subject.convert( 1 ) ).to eq( "one" )
+
+			expect( subject.convert( 2 ) ).to eq( "two" )
+
+			expect( subject.convert( 3 ) ).to eq( "three" )
+
+			expect( subject.convert( 4 ) ).to eq( "four" )
+
+			expect( subject.convert( 5 ) ).to eq( "five" )
+
+			expect( subject.convert( 6 ) ).to eq( "six" )
+
+			expect( subject.convert( 7 ) ).to eq( "seven" )
+
+			expect( subject.convert( 8 ) ).to eq( "eight" )
+
+			expect( subject.convert( 9 ) ).to eq( "nine" )
+
+			expect( subject.convert( 10 ) ).to eq( "ten" )
+
+		}
+
+	end
+
 end
