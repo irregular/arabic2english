1623b1ddd3702571f204c2bc5bcdca80a813e1ad
goreorto <cesar.cejudo@gmail.com>
2015-06-07 12:00:42 -0500

    Validates inputs and converts irregular numbers

--- lib/arabic/to/english.rb
+++ lib/arabic/to/english.rb
@@ -4,53 +4,114 @@ module Arabic
 
 		def convert number
 
-			case number
+			raise ArgumentError.new( "Integer number expected" ) unless number.is_a?( Fixnum )
 
-			when 1
+			if number <= 10
 
-				"one"
+				convert_decimal_base( number )
 
-			when 2
 
-				"two"
+			else
 
-			when 3
-
-				"three"
-
-			when 4
-
-				"four"
-
-			when 5
-
-				"five"
-
-			when 6
-
-				"six"
-
-			when 7
-
-				"seven"
-
-			when 8
-
-				"eight"
-
-			when 9
-
-				"nine"
-
-			when 10
-
-				"ten"
+				convert_irregular( number )
 
 			end
 
 		end
 
 		module_function :convert
+
+		class << self
+
+			def convert_decimal_base number
+
+				case number
+
+				when 1
+
+					"one"
+
+				when 2
+
+					"two"
+
+				when 3
+
+					"three"
+
+				when 4
+
+					"four"
+
+				when 5
+
+					"five"
+
+				when 6
+
+					"six"
+
+				when 7
+
+					"seven"
+
+				when 8
+
+					"eight"
+
+				when 9
+
+					"nine"
+
+				when 10
+
+					"ten"
+
+				end
+
+			end
+
+			def convert_irregular number
+
+				case number
+
+				when 11
+
+					"eleven"
+
+				when 12
+
+					"twelve"
+
+				when 13
+
+					"thirteen"
+
+				when 15
+
+					"fifteen"
+
+				when 20
+
+					"twelve"
+
+				when 30
+
+					"thirty"
+
+				when 40
+
+					"forty"
+
+				when 50
+
+					"fifty"
+
+				end
+
+			end
+
+		end
 
     end
   end
--- spec/arabic/to/english_spec.rb
+++ spec/arabic/to/english_spec.rb
@@ -2,7 +2,15 @@ require "arabic/to/english"
 
 describe Arabic::To::English do
 
-	describe "base number" do
+	it "rejects invalid inputs" do
+
+		expect { subject.convert( 'x' ) }.to raise_exception( ArgumentError )
+
+		expect { subject.convert( 1.2 ) }.to raise_exception( ArgumentError )
+
+	end
+
+	describe "decimal base" do
 
 		it {
 
@@ -27,6 +35,58 @@ describe Arabic::To::English do
 			expect( subject.convert( 10 ) ).to eq( "ten" )
 
 		}
+
+	end
+
+	describe "irregular numbers" do
+
+		it "11" do
+
+			expect( subject.convert( 11 ) ).to eq( "eleven" )
+
+		end
+
+		it "12" do
+
+			expect( subject.convert( 12 ) ).to eq( "twelve" )
+
+		end
+
+		it "13" do
+
+			expect( subject.convert( 13 ) ).to eq( "thirteen" )
+
+		end
+
+		it "15" do
+
+			expect( subject.convert( 15 ) ).to eq( "fifteen" )
+
+		end
+
+		it "20" do
+
+			expect( subject.convert( 20 ) ).to eq( "twelve" )
+
+		end
+
+		it "30" do
+
+			expect( subject.convert( 30 ) ).to eq( "thirty" )
+
+		end
+
+		it "40" do
+
+			expect( subject.convert( 40 ) ).to eq( "forty" )
+
+		end
+
+		it "50" do
+
+			expect( subject.convert( 50 ) ).to eq( "fifty" )
+
+		end
 
 	end
 
